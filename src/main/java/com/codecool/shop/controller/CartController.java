e7e9ce1ffd6d4c2724efb43187e8dfc338355d42
velicumadalina <velicumadalinaa@gmail.com>
2020-06-24 14:12:09 +0300

    Add quantities to shopping cart

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -11,19 +11,22 @@ import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.IContext;
 import org.thymeleaf.context.WebContext;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 
 @WebServlet(name = "CartController", urlPatterns = {"/cart"}, loadOnStartup = 1)
 public class CartController extends HttpServlet {
     private Cart myCart = new Cart();
     private float total = 0;
-
 
 
     @Override
@@ -42,8 +45,39 @@ public class CartController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, response, request.getServletContext());
-        context.setVariable("cart", myCart);
-        context.setVariable("total", total);
+        List<Integer> productQuantities = new ArrayList<>();
+        for (Product obj : myCart.getCartContent()) {
+            productQuantities.add(getQuantity(obj.getName()));
+        }
+        productQuantities = productQuantities.stream().distinct().collect(Collectors.toList());
+        System.out.println(productQuantities.toString());
+        List<Product> cart = myCart.getCartContent().stream().distinct().collect(Collectors.toList());
+        context.setVariable("cart", cart);
+        context.setVariable("totalPrice", total);
+        context.setVariable("quantities", productQuantities);
+        context.setVariable("totalItems", totalItems(productQuantities));
         engine.process("product/cart.html", context, response.getWriter());
+    }
+
+    public int getQuantity(String name) {
+        Product product = getProductByName(name);
+        return Collections.frequency(myCart.getCartContent(), product);
+    }
+
+    public Product getProductByName(String name){
+        for (Product prod : myCart.getCartContent()) {
+            if (prod.getName().equals(name)) {
+                return prod;
+            }
+        }
+        return null;
+    }
+
+    public int totalItems(List<Integer> cart) {
+        int total = 0;
+        for (Integer quantity : cart) {
+            total += quantity;
+        }
+        return total;
     }
 }
