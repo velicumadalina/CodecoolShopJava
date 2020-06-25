d2a98915edf7e3dee2203554dc78d8a54b0bed43
AdinaDida <adina_valentina10@yahoo.com>
2020-06-25 10:48:05 +0300

    Add check out form

---
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -0,0 +1,31 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CheckoutController", urlPatterns = {"/check-out"}, loadOnStartup = 1)
+public class CheckoutController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        engine.process("product/check_out.html", context, response.getWriter());
+    }
+
+
+}
