2be731201c70dea9cf0dfceae6ea9e04b4e14630
AdinaDida <adina_valentina10@yahoo.com>
2020-07-07 11:47:22 +0300

    Add payment details

--- src/main/java/com/codecool/shop/controller/PaymentController.java
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -1,8 +1,15 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.dao.implementation.OrderDaoMem;
+import com.codecool.shop.model.Order;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,31 +17,50 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
 @WebServlet(name = "PaymentController", urlPatterns = {"/payment"}, loadOnStartup = 1)
 public class PaymentController extends HttpServlet {
 
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
-
-//
-
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        OrderDao orderDao = OrderDaoMem.getInstance();
+        Order currentOrder = orderDao.find(1);
+        String order = currentOrder.getOrderInfo();
+        List<String> orderDetails = Arrays.asList(order.split("\\n"));
+        sendMail(currentOrder.getEmail(), "webshopemail123@gmail.com", order, currentOrder);
+        context.setVariable("orderDetails", orderDetails);
+        engine.process("product/details.html", context, response.getWriter());
     }
 
+    public void sendMail(String to, String from, String content, Order order) {
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.port", "587");
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, "123asd456fgh!");
+            }
+        });
 
-
-//    public String getOrderInfo(String name, String phone, String address, List<String> productDetails, String total){
-//        StringBuilder stringBuilder = new StringBuilder();
-//        stringBuilder.append(name).append(", thank you for your order!").append("\n").append(" Order summary: ");
-//        for(String elem:productDetails){
-//            stringBuilder.append(elem).append("\n");
-//        }
-//        stringBuilder.append("Total: ").append(total).append("\n");
-//        stringBuilder.append("Shipping information: ").append("\n"+name).append("\n"+phone).append("\n"+address).append("\n");
-//        stringBuilder.append("Thank you for choosing our shop!");
-//        return stringBuilder.toString();
-//    }
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Order #" + order.getId());
+            message.setText(content);
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
 
 
 }
