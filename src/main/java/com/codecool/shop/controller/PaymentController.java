520f41dd05bffdde7636931fd2f76ad88effd13d
velicumadalina <velicumadalinaa@gmail.com>
2020-07-07 10:11:22 +0300

    Fix add mail order details

--- src/main/java/com/codecool/shop/controller/PaymentController.java
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -1,9 +1,14 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
 @WebServlet(name = "PaymentController", urlPatterns = {"/payment"}, loadOnStartup = 1)
 public class PaymentController extends HttpServlet {
@@ -36,9 +42,52 @@ public class PaymentController extends HttpServlet {
         context.setVariable("address", address);
         context.setVariable("total", total);
         context.setVariable("productsDetails", productsDetails);
+        String orderInfo = getOrderInfo(name, phone, address, productsDetails, total);
+        sendMail(email, "webshopemail123@gmail.com", orderInfo);
+        System.out.println(orderInfo);
         engine.process("product/payment.html", context, response.getWriter());
     }
 
+    public void sendMail(String to, String from, String content) {
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.port", "587");
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, "123asd456fgh!");
+            }
+        });
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Order #1");
+            message.setText(content);
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+
+    public String getOrderInfo(String name, String phone, String address, List<String> productDetails, String total){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(name).append(", thank you for your order!").append("\n").append(" Order summary: ");
+        for(String elem:productDetails){
+            stringBuilder.append(elem).append("\n");
+        }
+        stringBuilder.append("Total: ").append(total).append("\n");
+        stringBuilder.append("Shipping information: ").append("\n"+name).append("\n"+phone).append("\n"+address).append("\n");
+        stringBuilder.append("Thank you for choosing our shop!");
+        return stringBuilder.toString();
+    }
 
 
 }
+
+
+
