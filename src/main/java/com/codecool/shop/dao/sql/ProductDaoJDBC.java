060a2b2d2c14bef7a34e9409df99520d2905cd21
AdinaDida <adina_valentina10@yahoo.com>
2020-07-09 14:17:10 +0300

    Add queries on ProductDaoJDBC

--- src/main/java/com/codecool/shop/dao/sql/ProductDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/sql/ProductDaoJDBC.java
@@ -1,17 +1,25 @@
 package com.codecool.shop.dao.sql;
 
 import com.codecool.shop.connection.dbConnection;
+import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDaoJDBC implements ProductDao {
+    private ProductCategoryDao productCategoryDao;
+    private SupplierDao supplierDao;
+
+
     @Override
     public void add(Product product) {
         String query = "INSERT INTO products (id, supplier_id, category_id, name, description, price, currency) " +
@@ -33,31 +41,136 @@ public class ProductDaoJDBC implements ProductDao {
 
     @Override
     public Product find(int id) {
-        return null;
+        Product tempProduct = null;
+        String query = "SELECT * FROM products WHERE id = ?";
+        try (Connection connection = dbConnection.getConnection();) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                tempProduct = new Product(
+                        resultSet.getString("name"),
+                        resultSet.getInt("price"),
+                        resultSet.getString("currency"),
+                        resultSet.getString("description"),
+                        productCategoryDao.find(resultSet.getInt("category_id")),
+                        supplierDao.find(resultSet.getInt("supplier_id")));
+                tempProduct.setId(resultSet.getInt("id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return tempProduct;
     }
 
     @Override
     public void remove(int id) {
-
+        String query = "DELETE FROM products WHERE id = ?";
+        try (Connection connection = dbConnection.getConnection();) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Override
     public List<Product> getAll() {
-        return null;
+        Product tempProduct;
+        List<Product> products = new ArrayList<>();
+        String query = "SELECT * FROM products";
+        try (Connection connection = dbConnection.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                tempProduct = new Product(
+                        resultSet.getString("name"),
+                        resultSet.getInt("price"),
+                        resultSet.getString("currency"),
+                        resultSet.getString("description"),
+                        productCategoryDao.find(resultSet.getInt("category_id")),
+                        supplierDao.find(resultSet.getInt("supplier_id")));
+                tempProduct.setId(resultSet.getInt("id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return products;
     }
 
     @Override
     public List<Product> getBy(Supplier supplier) {
-        return null;
+        Product tempProduct;
+        String query = "SELECT * FROM products WHERE supplier_id = ?";
+        List<Product> products = new ArrayList<>();
+        try (Connection connection = dbConnection.getConnection();) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, supplier.getId());
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                tempProduct = new Product(
+                        resultSet.getString("name"),
+                        resultSet.getInt("price"),
+                        resultSet.getString("currency"),
+                        resultSet.getString("description"),
+                        productCategoryDao.find(resultSet.getInt("category_id")),
+                        supplierDao.find(resultSet.getInt("supplier_id")));
+                tempProduct.setId(resultSet.getInt("id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return products;
     }
 
     @Override
     public List<Product> getBy(ProductCategory productCategory) {
-        return null;
+        Product tempProduct;
+        String query = "SELECT * FROM products WHERE category_id = ?";
+        List<Product> products = new ArrayList<>();
+        try (Connection connection = dbConnection.getConnection();) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, productCategory.getId());
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                tempProduct = new Product(
+                        resultSet.getString("name"),
+                        resultSet.getInt("price"),
+                        resultSet.getString("currency"),
+                        resultSet.getString("description"),
+                        productCategoryDao.find(resultSet.getInt("category_id")),
+                        supplierDao.find(resultSet.getInt("supplier_id")));
+                tempProduct.setId(resultSet.getInt("id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return products;
     }
 
     @Override
     public List<Product> filterBy(String category, String supplier) {
-        return null;
+        Product tempProduct;
+        String query = "SELECT * FROM products WHERE category_id = ? AND supplier_id = ?";
+        List<Product> products = new ArrayList<>();
+        try (Connection connection = dbConnection.getConnection();) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, productCategoryDao.getByName(category).getId());
+            statement.setInt(2, supplierDao.getByName(supplier).getId());
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                tempProduct = new Product(
+                        resultSet.getString("name"),
+                        resultSet.getInt("price"),
+                        resultSet.getString("currency"),
+                        resultSet.getString("description"),
+                        productCategoryDao.find(resultSet.getInt("category_id")),
+                        supplierDao.find(resultSet.getInt("supplier_id")));
+                tempProduct.setId(resultSet.getInt("id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return products;
     }
 }
