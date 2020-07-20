6b2d7870a1ecfd621e1fa508b2a91fa9ac87bf2d
AdinaDida <adina_valentina10@yahoo.com>
2020-07-20 15:25:15 +0300

    Add products

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -20,58 +20,74 @@ public class Initializer implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        ProductDao productDataStore =  new ProductDaoJDBC();
+        ProductCategoryDao productCategoryDataStore = new ProductCategoryDaoJDBC();
+        SupplierDao supplierDataStore = new SupplierDaoJDBC();
         CartDao cartDao = CartDaoMem.getInstance();
 //        OrderDao orderDao = OrderDaoMem.getInstance();
 
         //setting up a new supplier
-        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-        supplierDataStore.add(amazon);
-        Supplier lenovo = new Supplier("Lenovo", "Computers");
-        supplierDataStore.add(lenovo);
-        Supplier apple = new Supplier("Apple", "Apple");
-        supplierDataStore.add(apple);
-        Supplier samsung = new Supplier("Samsung", "Samsung");
-        supplierDataStore.add(samsung);
+        Supplier kiniCosmetics = new Supplier("Kini Cosmetics", "Kini");
+        kiniCosmetics.setId(1);
+        supplierDataStore.add(kiniCosmetics);
+        Supplier lush = new Supplier("Lush", "Lush");
+        lush.setId(2);
+        supplierDataStore.add(lush);
+        Supplier theSoapBar = new Supplier("The Soap Bar", "Soap");
+        theSoapBar.setId(3);
+        supplierDataStore.add(theSoapBar);
 
         //setting up a new product category
-        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        productCategoryDataStore.add(tablet);
-        ProductCategory phone = new ProductCategory("Phone", "Hardware", "A phone");
-        productCategoryDataStore.add(phone);
-        ProductCategory laptop = new ProductCategory("Laptop", "Hardware", "A laptop");
-        productCategoryDataStore.add(laptop);
+        ProductCategory soap = new ProductCategory("Soap", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        soap.setId(1);
+        productCategoryDataStore.add(soap);
+        ProductCategory candle = new ProductCategory("Candle", "Hardware", "A phone");
+        candle.setId(2);
+        productCategoryDataStore.add(candle);
+        ProductCategory bathBomb = new ProductCategory("Bath Bomb", "Hardware", "A laptop");
+        bathBomb.setId(3);
+        productCategoryDataStore.add(bathBomb);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, apple));
-        productDataStore.add(new Product("Samsung Galaxy S20 Ultra, Dual SIM", 50, "USD", "Galaxy S20 features a sleek modern design, next level capture and share and premium performance.", phone, samsung));
-        productDataStore.add(new Product("Apple iPhone 11, 64GB, Black", 150, "USD", "he iPhone 11 is the successor to last year’s iPhone XR model, with a 6.1-inch LCD display.", phone, apple));
-        productDataStore.add(new Product("Samsung Galaxy S10+", 350, "USD", "The Samsung Galaxy S10+ delivers flagship performance, it's slick and fast, happy to run intensive games at the top settings and does so without getting warm. ", phone, samsung));
-        productDataStore.add(new Product("Lenovo ThinkPad X1 Carbon 7th Gen", 250, "USD", "Business convertible for both homes and offices alike.", laptop, lenovo));
-        productDataStore.add(new Product("Lenovo ThinkPad E15", 350, "USD", "A sleek metallic design on top, performance underneath", laptop, lenovo));
-        productDataStore.add(new Product("Apple MacBook Pro 16inch Touch Bar", 450, "USD", "The best for the brightest", laptop, apple));
+        productDataStore.add(new Product("Orange Delight", 4.99f, "USD", "Homemade soap", soap, kiniCosmetics));
+        productDataStore.add(new Product("Calmness", 5.49f, "USD", "Homemade soap", soap, kiniCosmetics));
+        productDataStore.add(new Product("Java Blossom", 4.99f, "USD", "Homemade soap", soap, kiniCosmetics));
+        productDataStore.add(new Product("Honey Moon", 3.99f, "USD", "Homemade soap", soap, kiniCosmetics));
+        productDataStore.add(new Product("Hazelnut Tree", 4.99f, "USD", "Homemade soap", soap, lush));
+        productDataStore.add(new Product("Banana Split", 4.49f, "USD", "Homemade soap", soap, lush));
+        productDataStore.add(new Product("Starry Night", 4.99f, "USD", "Homemade soap", soap, lush));
+        productDataStore.add(new Product("Holy Guacamole", 5.99f, "USD", "Homemade soap", soap, theSoapBar));
+        productDataStore.add(new Product("Unicorn Days", 5.99f, "USD", "Homemade soap", soap, theSoapBar));
+        productDataStore.add(new Product("Queen Bee", 4.99f, "USD", "Homemade soap", soap, theSoapBar));
+        productDataStore.add(new Product("Summer Wine", 7.49f, "USD", "Homemade candle", candle, lush));
+        productDataStore.add(new Product("Petal Harmony", 7.99f, "USD", "Homemade candle", candle, lush));
+        productDataStore.add(new Product("Ruby Rose", 8.99f, "USD", "Homemade candle", candle, theSoapBar));
+        productDataStore.add(new Product("Cinnamon Rolls", 7.99f, "USD", "Homemade candle", candle, theSoapBar));
+        productDataStore.add(new Product("English Rose", 9.49f, "USD", "Homemade candle", candle, theSoapBar));
+        productDataStore.add(new Product("Purple Rain", 8.49f, "USD", "Homemade candle", candle, kiniCosmetics));
+        productDataStore.add(new Product("Cappuccino Dream", 2.49f, "USD", "Homemade bath bomb", bathBomb, theSoapBar));
+        productDataStore.add(new Product("Fluffy Marshmallow", 1.99f, "USD", "Homemade bath bomb", bathBomb, theSoapBar));
+        productDataStore.add(new Product("Fizzy Lemonade", 3.49f, "USD", "Homemade bath bomb", bathBomb, lush));
+        productDataStore.add(new Product("Crazy Watermelon", 2.49f, "USD", "Homemade bath bomb", bathBomb, lush));
+        productDataStore.add(new Product("Pokeball", 2.99f, "USD", "Homemade bath bomb", bathBomb, kiniCosmetics));
 
 
         //setting up the cart
         cartDao.add(new Cart(1));
 
-        //setting up supplier in db
-        SupplierDao supplierDaoJDBC = new SupplierDaoJDBC();
-        supplierDaoJDBC.add(apple);
-
-        //setting up product in db
-        ProductDao productDaoJDBC = new ProductDaoJDBC();
-        productDaoJDBC.add(new Product("Apple MacBook Pro 16inch Touch Bar", 450, "USD", "The best for the brightest", laptop, apple));
-        productDaoJDBC.add(new Product("Lenovo ThinkPad E15", 350, "USD", "A sleek metallic design on top, performance underneath", laptop, lenovo));
-
-        //setting up categories in db
-        ProductCategoryDao productCategoryDaoJDBC = new ProductCategoryDaoJDBC();
-        productCategoryDaoJDBC.add(phone);
-        productCategoryDaoJDBC.add(laptop);
+//        //setting up supplier in db
+//        SupplierDao supplierDaoJDBC = new SupplierDaoJDBC();
+//        supplierDaoJDBC.add(apple);
+//
+//        //setting up product in db
+//        ProductDao productDaoJDBC = new ProductDaoJDBC();
+//        productDaoJDBC.add(new Product("Apple MacBook Pro 16inch Touch Bar", 450, "USD", "The best for the brightest", laptop, apple));
+//        productDaoJDBC.add(new Product("Lenovo ThinkPad E15", 350, "USD", "A sleek metallic design on top, performance underneath", laptop, lenovo));
+//
+//        //setting up categories in db
+//        ProductCategoryDao productCategoryDaoJDBC = new ProductCategoryDaoJDBC();
+//        productCategoryDaoJDBC.add(phone);
+//        productCategoryDaoJDBC.add(laptop);
 
     }
 }
