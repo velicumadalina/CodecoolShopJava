35c1f967cd44eff2d3d09037a686409daf1bb8fa
AdinaDida <adina_valentina10@yahoo.com>
2020-07-24 06:37:21 +0300

    Add css style Refactor html

--- src/main/webapp/static/js/payment.js
+++ src/main/webapp/static/js/payment.js
@@ -5,7 +5,7 @@ function showPayment() {
     var text = document.getElementById("text");
 
     // If the checkbox is checked, display the output text
-    if (checkBox.checked == true){
+    if (checkBox.checked === true){
         text.style.display = "block";
     } else {
         text.style.display = "none";
